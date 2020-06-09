97f43141f7b7c059c0431fedc5d1fb479468534a
soluxos <callumharrod1994@hotmail.co.uk>
2020-06-09 19:54:35 +0100

    Added file that covers using functions

---
+++ 01_functions.py
@@ -0,0 +1,37 @@
+# Demonstration of using and creating functions in Python.
+
+# Variables
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+# Farenheit and Celsius
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * (5/9)
+  return c_temp
+
+f100_in_celsius = f_to_c(100)
+
+def c_to_f(c_temp):
+  f_temp = c_temp * (9/5) + 32
+  return f_temp
+c0_in_fahrenheit = c_to_f(0)
+
+# Using the force
+def get_force(mass, acceleration):
+  return mass * acceleration
+train_force = get_force(train_mass, train_acceleration)
+print("The GE train supplies " + str(train_force) + " Newtons of force.")
+
+# Get energy
+def get_energy(mass, c=3*10**8):
+  return mass * c
+bomb_energy = get_energy(bomb_mass)
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules")
+
+# Get work
+def get_work(mass, acceleration, distance):
+  return get_force(mass, acceleration) * distance
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + " meters.")
